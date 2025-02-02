305f80b4aac917766cf38f6c1a41da38026c174a
JVPC <jeremellev@icloud.com>
2025-02-02 15:48:05 -0700

    Added user select to signup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.chalkitup"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 24 //Minimum android version
+        targetSdk = 35 //latest tested version
         versionCode = 1
         versionName = "1.0"
 
