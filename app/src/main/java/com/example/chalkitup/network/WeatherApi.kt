39c85ec15d47c4e571548f98d2e076e56336c54d
K-littlejohn <littlejohnk2@mymacewan.ca>
2025-03-07 18:59:33 -0700

    WeatherApi

---
+++ app/src/main/java/com/example/chalkitup/network/WeatherApi.kt
@@ -0,0 +1,13 @@
+package com.example.chalkitup.network
+
+import com.example.chalkitup.model.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("v1/current.json")
+    suspend fun getWeather(
+        @Query("key") apiKey: String,
+        @Query("q") location: String
+    ): WeatherResponse
+}
