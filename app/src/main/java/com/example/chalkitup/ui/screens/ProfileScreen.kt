0663fdd1aa9ecf1869a1b5f9185fd29576543933
JVPC <jeremellev@icloud.com>
2025-03-24 18:10:01 -0600

    Changed file icon for certifications that are not images

--- app/src/main/java/com/example/chalkitup/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/chalkitup/ui/screens/ProfileScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 //import androidx.compose.material3.Button
 //import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
